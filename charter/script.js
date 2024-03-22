2f36c4000cdfd5b6e6e17bbef1179c0c779e7a80
Nikhil-Sultania <155643263+Nikhil-Sultania@users.noreply.github.com>
2024-03-23 01:09:55 +0800

    MVP up, requires depression tracking and Statistical analysis

--- charter/script.js
+++ charter/script.js
@@ -1,4 +1,4 @@
-const margin = {top: 20, right: 20, bottom: 30, left: 40};
+const margin = {top: 20, right: 20, bottom: 30, left: 70};
 const width = 960 - margin.left - margin.right;
 const height = 500 - margin.top - margin.bottom;
 
@@ -49,39 +49,70 @@ function chart(dataset) {
 
 
 
-    function chart(...datasets) {
-        const promises = datasets.map(d => d3.json(d)); // Load all datasets asynchronously
+function chart(...datasets) {
+    const promises = datasets.map(d => d3.json(d)); // Load all datasets asynchronously
 
-        Promise.all(promises).then(data => {
-            // Combine data processing for all datasets
-            const parseTime = d3.timeParse("%Y-%m-%d");
-            data.forEach(dataset => dataset.forEach(d => d.date = parseTime(d.date)));
+    Promise.all(promises).then(data => {
+        // Combine data processing for all datasets
+        const parseTime = d3.timeParse("%Y-%m-%d");
+        data.forEach(dataset => dataset.forEach(d => d.date = parseTime(d.date)));
 
-            // Determine combined domain for x and y scales
-            const xDomain = d3.extent(data.flat(), d => d.date);
-            const yDomain = [0, d3.max(data.flat(), d => d.value)];
+        // Determine combined domain for x and y scales
+        const xDomain = d3.extent(data.flat(), d => d.date);
+        const yDomain = [0, d3.max(data.flat(), d => d.value)];
 
-            const xScale = d3.scaleTime().domain(xDomain).range([0, width]);
-            const yScale = d3.scaleLinear().domain(yDomain).range([height, 0]);
+        // Define xScale and yScale here for accessibility
+        const xScale = d3.scaleTime().domain(xDomain).range([0, width]);
+        const yScale = d3.scaleLinear().domain(yDomain).range([height, 0]);
 
-            // Create and append paths for each dataset with distinct colors
-            data.forEach((dataset, index) => {
-                const line = d3.line().x(d => xScale(d.date)).y(d => yScale(d.value));
+        // Create tooltip element
+        const tooltip = d3.select(".tooltip");
 
-                svg.append("path")
-                    .datum(dataset)
-                    .attr("fill", "none")
-                    .attr("stroke", d3.schemeCategory10[index]) // Use color from a palette
-                    .attr("stroke-width", 1.5)
-                    .attr("d", line);
-            });
+        // Create and append paths for each dataset with distinct colors
+        data.forEach((dataset, index) => {
+            const line = d3.line().x(d => xScale(d.date)).y(d => yScale(d.value));
 
-            // Add axes (same as before)
-            svg.append("g")
-                .attr("transform", `translate(0,${height})`)
-                .call(d3.axisBottom(xScale));
+            svg.append("path")
+                .datum(dataset)
+                .attr("fill", "none")
+                .attr("stroke", d3.schemeCategory10[index]) // Use color from a palette
+                .attr("stroke-width", 1.5)
+                .attr("d", line)
+                .on("mouseover", function(event,d) { // Add mouseover event handler
+                    const mouseX = event.pageX;
+                    const mouseY = event.pageY;
 
-            svg.append("g")
-                .call(d3.axisLeft(yScale));
+                    // Find the closest data point to the mouse position
+                    const closestDataPoint = dataset.find(point => {
+                        const distance = Math.abs(xScale(point.date) - mouseX);
+                        return distance < 10; // Adjust threshold as needed
+                    });
+
+
+                    if (closestDataPoint) {
+                        const svgRect = svg.node().getBoundingClientRect();
+                        tooltip.transition()
+                            .duration(200)
+                            .style("opacity", 1)
+                            .style("color", d3.schemeCategory10[index])
+                        tooltip.html(`Date: ${closestDataPoint.date.toLocaleDateString()}<br>Value: ${closestDataPoint.value}`)
+                            .style("left", (mouseX + 15) +'px')
+                            .style("top", (mouseY +  15) +'px');
+                    }
+                })
+                .on("mouseout", function() {
+                    tooltip.transition()
+                        .duration(500)
+                        .style("opacity", 0);
+                });
         });
-    }
+
+        // Add axes
+        svg.append("g")
+            .attr("transform", `translate(0,${height})`)
+            .call(d3.axisBottom(xScale));
+
+        svg.append("g")
+            .call(d3.axisLeft(yScale));
+    });
+}
